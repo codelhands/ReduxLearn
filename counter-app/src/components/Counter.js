3bf95da8750b619d0266ab631ebd1e71a9497da5
Elif <elffemre@gmail.com>
2022-08-02 15:33:02 +0300

    redux - son

--- counter-app/src/components/Counter.js
+++ counter-app/src/components/Counter.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
-import {useSelector} from 'react-redux'; //useSelector = hook
+import {useSelector,useDispatch} from 'react-redux'; //useSelector = hook
+
+import {increase,decrease} from '../redux/counter/counterSlice';
 
 function Counter() { 
     //countValue içine yazılan fonskiyon bir callback fonksiyonudur
     const countValue = useSelector((state)=> state.counter.value);
 
+    const dispatch = useDispatch();
+
   return (
     <div>
     <h1>{countValue}</h1>
+
+
+    <button onClick={() => dispatch(increase())}>Increase</button>
+    <button onClick={() => dispatch(decrease())}>Decrease</button>
     </div>
   )
 }
--- counter-app/src/redux/counter/counterSlice.js
+++ counter-app/src/redux/counter/counterSlice.js
@@ -9,11 +9,18 @@ import { createSlice } from "@reduxjs/toolkit"; // slice oluştur
 export const counterSlice = createSlice({
     name: 'counter', //state' ulaşmak istediğimiz zaman state.counter olarak gitmemizi sağlayacak
     initialState: { //state'in ilk oluşturulurken ki değerlerini belirlemek için
-        value: 0 //veri tanımlama
+    value: 0 //veri tanımlama
     },
     reducers: { //bu bir field'dır. bunun içine stateleri güncelleyeceğimiz şeyleri yazacağız
+        increase: (state) => {
+            state.value += 1; //yukaridaki value
+        },
+        decrease: (state) => {
+            state.value -=1;
+        }
     
     },
 });
-
+export const {increase} = counterSlice.actions;
+export const {decrease} = counterSlice.actions;
 export default counterSlice.reducer;
